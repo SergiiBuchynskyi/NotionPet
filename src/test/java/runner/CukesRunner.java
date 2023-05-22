0e738e8a82e5e26f632b773028231ed8eef11041
Sergii Buchynskyi <Buchik_zt@hotmail.com>
2023-05-22 14:12:11 +0300

    Initial commit Created Project POM Browser Utils Conf Reader Driver Runner

---
+++ src/test/java/runner/CukesRunner.java
@@ -0,0 +1,22 @@
+package Runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "json:target/cucumber.json", //this one to connect with jira
+                "html:target/cucumber-reports.html",
+                "rerun:target/rerun.txt"
+
+        },
+
+        features ="src/test/resources/features",
+        glue ="Step_Definitions",
+        dryRun=false,
+        tags = "@SalesFunctionality"
+)
+public class CukesRunner {
+}
